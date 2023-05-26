38eaeec0f096334052b2c63dc69203e2f360321c
Robinson Rodriguez <126893454+robert-3r@users.noreply.github>
2023-05-26 16:12:03 -0500

    falta poco

--- constansts.js
+++ constansts.js
@@ -3,7 +3,7 @@ const DB_PASSWORD = "todo23";
 const DB_HOST = "todolist.6gthigi.mongodb.net";
 
 const API_VERSION = "v1";
-const IP_SERVER = "localhost";
+const IP_SERVER = "doca-web.onrender.com";
 
 const JWT_SECRET_KEY = '2F9#z4N7^jW!mPq$uS3hA&vK6yE@rG5dXc8Tb%gY1xL*pO0nIwCfB$zH3jU7eR1sQ9aZ5tM2oV6ñññ3wefeecpepe23todo'
 
